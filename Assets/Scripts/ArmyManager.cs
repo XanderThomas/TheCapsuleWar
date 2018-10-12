a145626a06999205ba4c3abc13fe4ff7f229907c
XanderThomas <ax.n.thomas@gmail.com>
2018-10-12 09:58:18 -0700

    Add ArmyManager & ArmyUnit scripts

---
+++ Assets/Scripts/ArmyManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmyManager : MonoBehaviour {
+
+    [SerializeField]
+    private Transform spawn;
+    [SerializeField]
+    private GameObject tempTestUnitPrefab;
+    [SerializeField]
+    private ArmyManager enemyArmyManager;
+    [SerializeField]
+    private float spawnZRange;
+
+    //armyFront is distance from spawn, not a global space position
+    public float armyFront { get; private set; }
+    //unitStartX is a global space position
+    public float unitStartX { get; private set; }
+
+    private List<ArmyUnit> units = new List<ArmyUnit>();
+
+
+
+
+
+    private void Awake()
+    {
+        unitStartX = spawn.position.x;
+
+        InvokeRepeating("TEMP_SpawnUnit", 0.1f, 2f);
+    }
+
+    private void TEMP_SpawnUnit()
+    {
+        GameObject unit = Instantiate(tempTestUnitPrefab);
+        Vector3 spawnPos = spawn.position;
+        spawnPos.z += Random.Range(-spawnZRange, spawnZRange);
+        unit.transform.position = spawnPos;
+        unit.transform.rotation = spawn.rotation;
+
+        ArmyUnit script = unit.GetComponent<ArmyUnit>();
+        script.allyManager = this;
+        script.enemyManager = enemyArmyManager;
+
+        units.Add(script);
+    }
+
+    private void Update()
+    {
+        float dt = Time.deltaTime;
+        float furthestUnitDist = 0f;
+
+        for(int i = 0; i < units.Count; ++i)
+        {
+            units[i].ManagerUpdate(dt);
+            if (units[i].distFromSpawn > furthestUnitDist)
+                furthestUnitDist = units[i].distFromSpawn;
+        }
+
+        armyFront = furthestUnitDist;
+    }
+
+}
---
+++ Assets/Scripts/ArmyUnit.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmyUnit : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float attackRange;
+
+    [HideInInspector]
+    public ArmyManager allyManager;
+    [HideInInspector]
+    public ArmyManager enemyManager;
+
+    public float distFromSpawn { get; private set; }
+    public float distFromEnemySpawn { get; private set; }
+
+    private float moveDir;
+
+
+
+    private void Start()
+    {
+        distFromSpawn = 0f;
+
+        Vector3 pos = transform.position;
+        if(enemyManager.unitStartX > pos.x)
+        {
+            distFromEnemySpawn = enemyManager.unitStartX - pos.x;
+            moveDir = 1f;
+        }
+        else
+        {
+            distFromEnemySpawn = pos.x - enemyManager.unitStartX;
+            moveDir = -1f;
+        }
+    }
+
+    public void ManagerUpdate(float dt)
+    {
+        if(distFromEnemySpawn > enemyManager.armyFront + attackRange)
+        {
+            float moveDist = moveSpeed * dt;
+
+            transform.position += new Vector3(moveDist * moveDir, 0f, 0f);
+            distFromSpawn += moveDist;
+            distFromEnemySpawn -= moveDist;
+        }
+    }
+
+}
