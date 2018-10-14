0df886d75199ec5f5796b9e5c0863403e6f17f5c
Alexander Thomas <6ftDwarf@gmail.com>
2018-10-13 17:16:46 -0700

    Add rudimentary enemy team controller

---
+++ Assets/Scripts/TeamControllers/BasicEnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyController : MonoBehaviour {
+
+    [SerializeField]
+    private ArmyManager armyManager;
+    [SerializeField]
+    private ResourceManager resourceManager;
+    [SerializeField]
+    private ArmyManager enemyArmyManager;
+
+
+
+    private void Update()
+    {
+        armyManager.BuyUnit(0);
+    }
+
+}
