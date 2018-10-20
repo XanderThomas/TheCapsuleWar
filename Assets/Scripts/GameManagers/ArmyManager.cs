fa98df537684cb5a036718758fe3a3aaf7fcf888
Alexander Thomas <6ftDwarf@gmail.com>
2018-10-20 15:19:30 -0700

    Add commenting to ArmyManager

--- Assets/Scripts/GameManagers/ArmyManager.cs
+++ Assets/Scripts/GameManagers/ArmyManager.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Manages a team's current units & the list of units it can spawn
+/// </summary>
 [RequireComponent(typeof(ResourceManager))]
 public class ArmyManager : MonoBehaviour {
 
+    //Struct for a unit spawn type & cost
     [System.Serializable]
     public struct ArmySpawn
     {
@@ -12,27 +16,33 @@ public class ArmyManager : MonoBehaviour {
         public GameObject prefab;
     }
 
-    [SerializeField]
+    [SerializeField, Tooltip("A list of all the unit types this army can spawn")]
     private ArmySpawn[] spawnableUnits;
-    [SerializeField]
+    [SerializeField, Tooltip("The position & rotation spawned units will start at")]
     private Transform spawn;
-    [SerializeField]
-    private GameObject tempTestUnitPrefab;
-    [SerializeField]
+    [SerializeField, Tooltip("The ArmyManager for the enemy team")]
     private ArmyManager enemyArmyManager;
-    [SerializeField]
+    [SerializeField, Tooltip("The length of the line alone which units will be spawned")]
     private float spawnZRange;
-    [SerializeField]
+    [SerializeField, Tooltip("The tag given to spawned units' GameObjects")]
     private string teamUnitTag;
-    [SerializeField]
+    [SerializeField, Tooltip("This team's ResourceManager")]
     private ResourceManager resourceManager;
 
     //armyFront is distance from spawn, not a global space position
+    /// <summary>
+    /// The furthest absolute Z-axis distance any unit of this army currently is from spawn
+    /// </summary>
     public float armyFront { get; private set; }
-    //unitStartX is a global space position
+    /// <summary>
+    /// The global X position units are spawned at
+    /// </summary>
     public float unitStartX { get; private set; }
+    /// <summary>
+    /// The tag given to spawned units' GameObjects
+    /// </summary>
     public string TeamUnitTag { get { return teamUnitTag; } }
-
+    
     private List<ArmyUnit> units = new List<ArmyUnit>();
 
 
@@ -41,8 +51,9 @@ public class ArmyManager : MonoBehaviour {
 
     private void Awake()
     {
+        //Set the publicly accessible property for unit starting position
         unitStartX = spawn.position.x;
-
+        
 #if DEBUG
         if (!resourceManager)
             Debug.LogError("ArmyManager " + gameObject.name + " resourceManager not set!");
@@ -54,16 +65,21 @@ public class ArmyManager : MonoBehaviour {
         float dt = Time.deltaTime;
         float furthestUnitDist = 0f;
 
+        //Update all units & determine the distance of the furthest from spawn
         for(int i = 0; i < units.Count; ++i)
         {
             units[i].ManagerUpdate(dt);
             if (units[i].distFromSpawn > furthestUnitDist)
                 furthestUnitDist = units[i].distFromSpawn;
         }
-
+        
         armyFront = furthestUnitDist;
     }
 
+    /// <summary>
+    /// Remove unit from internal list & destroy its gameobject
+    /// </summary>
+    /// <param name="unit"></param>
     public void KillUnit(ArmyUnit unit)
     {
         units.Remove(unit);
@@ -79,6 +95,11 @@ public class ArmyManager : MonoBehaviour {
         BuyUnit(idx);
     }
 
+    /// <summary>
+    /// Attempt to spawn unit & spend appropriate resources
+    /// </summary>
+    /// <param name="idx">Index of the unit to spawn in spawnableUnits array</param>
+    /// <returns></returns>
     public bool BuyUnit(int idx)
     {
         if (resourceManager.SpendResources(spawnableUnits[idx].cost))
@@ -91,6 +112,10 @@ public class ArmyManager : MonoBehaviour {
             return false;
     }
 
+    /// <summary>
+    /// Create and initialize a unit
+    /// </summary>
+    /// <param name="idx">Index of the unit to spawn in spawnableUnits array</param>
     private void SpawnUnit(int idx)
     {
         GameObject unit = Instantiate(spawnableUnits[idx].prefab);
