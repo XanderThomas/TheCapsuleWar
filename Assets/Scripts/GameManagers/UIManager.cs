33c67835a6f7dc3e1dd2f2b4a6412398b73bc7e2
Alexander Thomas <6ftDwarf@gmail.com>
2018-10-20 14:23:21 -0700

    Add commenting & tooltips to UIManager

--- Assets/Scripts/GameManagers/UIManager.cs
+++ Assets/Scripts/GameManagers/UIManager.cs
@@ -7,54 +7,57 @@ public class UIManager : MonoBehaviour {
 
     [Header("Data sources")]
 
-    [SerializeField]
+    [SerializeField, Tooltip("The ResourceManager script for the player's resources")]
     private ResourceManager playerResourceManager;
 
     [Header("Settings")]
 
-    [SerializeField]
+    [SerializeField, Tooltip("The text to display if the player wins")]
     private string playerWinText;
-    [SerializeField]
+    [SerializeField, Tooltip("The text to display if the enemy wins")]
     private string enemyWinText;
 
     [Header("UI elements")]
 
-    [SerializeField]
+    [SerializeField, Tooltip("This will display the amount of gold the player has as a number")]
     private Text goldText;
-    [SerializeField]
+    [SerializeField, Tooltip("This needs to be an image whose transform is stretched to fill its parent. It will display the amount of gold the palyer has as a color bar")]
     private RectTransform goldBar;
-    [SerializeField]
+    [SerializeField, Tooltip("This needs to be the parent RectTransform of Gold Bar above")]
     private RectTransform goldBarHolder;
 
-    [SerializeField]
+    [SerializeField, Tooltip("This will display the amount of wood the player has as a number")]
     private Text woodText;
-    [SerializeField]
+    [SerializeField, Tooltip("This needs to be an image whose transform is stretched to fill its parent. It will display the amount of wood the palyer has as a color bar")]
     private RectTransform woodBar;
-    [SerializeField]
+    [SerializeField, Tooltip("This needs to be the parent RectTransform of Wood Bar above")]
     private RectTransform woodBarHolder;
 
-    [SerializeField]
+    [SerializeField, Tooltip("This will display the amount of iron the player has as a number")]
     private Text ironText;
-    [SerializeField]
+    [SerializeField, Tooltip("This needs to be an image whose transform is stretched to fill its parent. It will display the amount of iron the palyer has as a color bar")]
     private RectTransform ironBar;
-    [SerializeField]
+    [SerializeField, Tooltip("This needs to be the parent RectTransform of Iron Bar above")]
     private RectTransform ironBarHolder;
 
-    [SerializeField]
+    [SerializeField, Tooltip("This is the text component which will display Player or Enemy Win Text from above. Its gameobject will be inactive most of the game")]
     private Text gameOverText;
 
 
 
     private void Awake()
     {
+        //Hide the game over text object at the start of the game
         gameOverText.gameObject.SetActive(false);
     }
 
     private void Update()
     {
+        //Get the player's current resource & resource capacity amounts
         ResourceTypes resources = playerResourceManager.Resources;
         ResourceTypes resourceCapacity = playerResourceManager.ResourceCapacity;
-
+        
+        //Display the player's current resources & resource capacity
         goldText.text = "Gold: " + resources.gold;
         goldBar.offsetMax = new Vector2(-goldBarHolder.sizeDelta.x +
             (resources.gold / (float)resourceCapacity.gold) * goldBarHolder.sizeDelta.x, 0f);
@@ -68,6 +71,10 @@ public class UIManager : MonoBehaviour {
             (resources.iron / (float)resourceCapacity.iron) * ironBarHolder.sizeDelta.x, 0f);
     }
 
+    /// <summary>
+    /// Cause the UIManager to display game over text
+    /// </summary>
+    /// <param name="playerWins">True if player won, false if enemy won</param>
     public void DisplayGameOver(bool playerWins)
     {
         gameOverText.gameObject.SetActive(true);
