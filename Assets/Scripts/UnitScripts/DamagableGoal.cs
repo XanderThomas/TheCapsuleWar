91e439e4531f61518e64f1e89bf79bfaa001ab59
Alexander Thomas <6ftDwarf@gmail.com>
2018-10-20 14:34:09 -0700

    Add commenting & tooltips to DamagableGoal

--- Assets/Scripts/UnitScripts/DamagableGoal.cs
+++ Assets/Scripts/UnitScripts/DamagableGoal.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A DamagableAbstract component which will cause the game to end when it dies
+/// </summary>
 public class DamagableGoal : DamagableAbstract {
 
-    [SerializeField]
+    [SerializeField, Tooltip("Is this the player's goal?")]
     private bool playerGoal;
-    [SerializeField]
+    [SerializeField, Tooltip("A link to the scene's GameOverManager")]
     private GameOverManager gameOverManager;
 
 
 
+    /// <summary>
+    /// Reduces hp by amt and clamps hp from 0 to maxHP, and causes game to end if hp==0
+    /// </summary>
+    /// <param name="amt">Quantity of damage to take, or negative value to heal</param>
     public override void TakeDamage(float amt)
     {
         base.TakeDamage(amt);
